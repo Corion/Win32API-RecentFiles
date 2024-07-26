56a9b9fbaadccf59de25fa5a798ded76dd0e7c55
Max Maischein <corion@corion.net>
2024-07-26 19:59:19 +0200

    More documentation

--- lib/Win32API/RecentFiles.pm
+++ lib/Win32API/RecentFiles.pm
@@ -20,8 +20,19 @@ Win32API::RecentFiles - recently accessed file API functions on Windows
 
 =head1 SYNOPSIS
 
-  use Win32API::RecentFiles qw(SHAddToREcentDocsA SHAddToRecentDocsW);
+  use Win32API::RecentFiles qw(SHAddToRecentDocsA SHAddToRecentDocsW);
+  use Win32;
   SHAddToRecentDocsA('C:\\Full\\Path\\To\\Makefile.PL');
   SHAddToRecentDocsW('C:\\Full\\Path\\To\\fünf.txt');
+  my $recent_dir = Win32::GetFolderPath(Win32::CSIDL_RECENT());
+  # $recent_dir\\fünf.txt.lnk exists
+
+=head1 DESCRIPTION
+
+This module exports the SHAddToRecentDocsA and SHAddToRecentDocsW functions.
+
+=head1 SEE ALSO
+
+Microsoft documentation at L<https://learn.microsoft.com/de-de/windows/win32/api/shlobj_core/nf-shlobj_core-shaddtorecentdocs>
 
 =cut

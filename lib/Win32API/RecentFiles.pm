09b48af698fd15d648d49b27b63ce25a199205dd
Max Maischein <corion@corion.net>
2024-07-26 19:27:50 +0200

    Initial check-in

---
+++ lib/Win32API/RecentFiles.pm
@@ -0,0 +1,13 @@
+package Win32API::RecentFiles;
+use 5.020;
+use experimental 'signatures';
+use Exporter 'import';
+require DynaLoader;
+our @ISA = qw(Exporter DynaLoader);
+
+our @EXPORT_OK = qw(SHAddToRecentDocsA SHAddToRecentDocsW);
+
+our $VERSION = '0.01';
+
+bootstrap Win32API::RecentFiles;
+1;
